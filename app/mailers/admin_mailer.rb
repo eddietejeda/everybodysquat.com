2b084210aeeff544ecdeabf7bd29a6260620518b
Eddie A Tejeda <eddie.tejeda@gmail.com>
2019-03-02 20:33:33 -0500

    add debug message

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -7,6 +7,7 @@ class AdminMailer < ApplicationMailer
   def new_user_waiting_for_approval(email)
     @email = email
     to_email = ENV['ADMIN_EMAIL'] || "#{Etc.getlogin}@localhost"
+    logger.debug "Admin email is: #{to_email}"
     mail(to: to_email, subject: 'New user awaiting approval')
   end
 end
