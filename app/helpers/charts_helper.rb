7f244c03901c91b26d6f3bd658e800166595deb3
Eddie A Tejeda <eddie.tejeda@gmail.com>
2019-03-31 14:08:12 -0400

    limit tracking of lifts to after usre completes 4 workouts

--- app/helpers/charts_helper.rb
+++ app/helpers/charts_helper.rb
@@ -3,8 +3,13 @@
 module ChartsHelper
   
   def render_progress_chart
-    content_tag(:div, class: "chart", data: {controller: 'chart'}) do
-      content_tag(:canvas, "", class: "show", data: {target: 'chart.show'} )
+    
+    if current_user.workouts.count > 4
+      content_tag(:div, class: "chart", data: {controller: 'chart'}) do
+        content_tag(:canvas, "", class: "show", data: {target: 'chart.show'} )
+      end
+    else
+      content_tag(:div, content_tag(:p, "Charts will appear after you complete four workouts"), class: "strong")      
     end
   end
 
