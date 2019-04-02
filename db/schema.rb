f33a716ab5961213382e952bd35635547e0b38a9
Eddie A Tejeda <eddie.tejeda@gmail.com>
2019-04-01 22:23:46 -0400

    run migrate to drop body

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_31_190602) do
+ActiveRecord::Schema.define(version: 2019_04_02_022209) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -100,6 +100,7 @@ ActiveRecord::Schema.define(version: 2019_03_31_190602) do
     t.datetime "updated_at", null: false
     t.string "username"
     t.boolean "approved", default: false, null: false
+    t.jsonb "details"
     t.index ["approved"], name: "index_users_on_approved"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
