27211a2ab707d6ba6074e4df5bae6e63c03bdfd1
Eddie A Tejeda <eddie.tejeda@gmail.com>
2019-04-29 00:19:35 -0400

    when targetEnvironment is simulator, use localhost, otherwise, live url

---
+++ ios/EveryBodySquat/EveryBodySquat/AppDelegate.swift
@@ -0,0 +1,58 @@
+//
+//  AppDelegate.swift
+//  EveryBodySquat
+//
+//  Created by Eddie A Tejeda on 2/23/19.
+//  Copyright © 2019 Visudo. All rights reserved.
+//
+
+import UIKit
+import WebKit
+import Turbolinks
+
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    var navigationController =  UINavigationController()
+    lazy var session: Session = {
+        let configuration = WKWebViewConfiguration()
+        configuration.applicationNameForUserAgent = "EveryBodySquat iOS"
+        return Session( webViewConfiguration: configuration )
+    }()
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        navigationController.setNavigationBarHidden(true, animated: true)
+        window?.rootViewController = navigationController
+        session.delegate = self
+        
+        #if targetEnvironment(simulator)
+        visit(URL: URL(string: "http://localhost:3000")!)
+        #else
+        visit(URL: URL(string: "https://www.everybodysquat.com")!)
+        #endif
+        return true
+    }
+
+    
+    func visit(URL: URL){
+        let viewController = VisitableViewController(url: URL)
+        navigationController.pushViewController(viewController, animated: true)
+        session.visit(viewController)
+    }
+
+}
+
+
+extension AppDelegate: SessionDelegate {
+    func session(_ session: Session, didFailRequestForVisitable visitable: Visitable, withError error: NSError) {
+        //
+    }
+    
+    func session(_ session: Session, didProposeVisitToURL URL: URL, withAction action: Action) {
+        visit(URL: URL)
+    }
+    
+
+}
